9b313a6a42204133d8a65289e10b3ecdc945dbd5
frog <dickycahyakurniawan98@gmail.com>
2025-09-13 20:11:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
-async function loadMovies() {
-  try {
-    const res = await fetch('data/movies.json');
-    const movies = await res.json();
-    window.allMovies = movies;
-    displayMovies(movies);
-  } catch (err) {
-    document.getElementById('movie-list').innerText = 'Error: Gagal memuat data film';
-  }
+// Render daftar film
+const movieList = document.getElementById('movieList');
+
+function renderMovies(list) {
+  movieList.innerHTML = "";
+  list.forEach(movie => {
+    const div = document.createElement("div");
+    div.className = "movie";
+    div.setAttribute("data-genre", movie.genre);
+    div.innerHTML = `
+      <img src="${movie.poster}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+    `;
+    div.onclick = () => window.open(movie.url, "_blank");
+    movieList.appendChild(div);
+  });
 }
 
-function displayMovies(movies) {
-  const list = document.getElementById('movie-list');
-  if (!movies.length) {
-    list.innerHTML = '<p>Film tidak ditemukan.</p>';
-    return;
-  }
-  list.innerHTML = movies.map(m => `
-    <div class="movie-card" onclick="goToFilm('${m.id}')">
-      <img src="${m.poster}" alt="${m.title}" loading="lazy">
-      <h3>${m.title}</h3>
-      <p>${m.genre}</p>
-    </div>`).join('');
-}
-
-function filterGenre(genre) {
-  if (genre === 'All') {
-    displayMovies(window.allMovies);
+// Filter genre
+function filterMovies(genre) {
+  if (genre === "all") {
+    renderMovies(movies);
   } else {
-    displayMovies(window.allMovies.filter(m => m.genre === genre));
+    renderMovies(movies.filter(m => m.genre === genre));
   }
 }
 
-function searchMovie() {
-  const q = document.getElementById('searchInput').value.toLowerCase();
-  displayMovies(window.allMovies.filter(m => m.title.toLowerCase().includes(q)));
+// Pencarian
+function searchMovies() {
+  const input = document.getElementById('searchInput').value.toLowerCase();
+  const filtered = movies.filter(m => m.title.toLowerCase().includes(input));
+  renderMovies(filtered);
 }
 
-function goToFilm(id) {
-  window.location.href = `film.html?id=${id}`;
-}
-
-async function loadFilmDetail() {
-  const params = new URLSearchParams(window.location.search);
-  const id = params.get('id');
-  if (!id) return;
-
-  try {
-    const res = await fetch('data/movies.json');
-    const movies = await res.json();
-    const movie = movies.find(m => m.id === id);
-    const detail = document.getElementById('film-detail');
-
-    if (movie) {
-      detail.innerHTML = `
-        <h2>${movie.title}</h2>
-        <img src="${movie.poster}" alt="${movie.title}" style="max-width:200px" loading="lazy">
-        <p><b>Genre:</b> ${movie.genre}</p>
-        <p>${movie.description}</p>
-      `;
-
-      // Tambahkan embed video jika ada
-      if (movie.video) {
-        detail.innerHTML += `
-          <div style="margin-top:20px">
-            <iframe width="560" height="315" 
-              src="${movie.video}" 
-              frameborder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowfullscreen>
-            </iframe>
-          </div>`;
-      }
-    } else {
-      detail.innerHTML = '<p>Film tidak ditemukan.</p>';
-    }
-  } catch (err) {
-    document.getElementById('film-detail').innerText = 'Error: gagal memuat detail film';
-  }
-}
-
-if (document.getElementById('movie-list')) loadMovies();
-if (document.getElementById('film-detail')) loadFilmDetail();
+// Default tampil semua film
+renderMovies(movies);
