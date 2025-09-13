9fbe73afb93d5910ac52af550def74a10cb24339
frog <dickycahyakurniawan98@gmail.com>
2025-09-13 20:38:23 +0700

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,21 @@
+const movies = [
+  {
+    title: "Legend of Hero 1",
+    genre: "action",
+    poster: "posters/movie1.jpg",
+    url: "https://youtu.be/dQw4w9WgXcQ"
+  },
+  {
+    title: "Romance in Paris",
+    genre: "romance",
+    poster: "posters/movie2.jpg",
+    url: "https://youtu.be/dQw4w9WgXcQ"
+  },
+  {
+    title: "Dragon Fantasy",
+    genre: "fantasy",
+    poster: "posters/movie3.jpg",
+    url: "https://youtu.be/dQw4w9WgXcQ"
+  }
+  // tambahin seterusnya...
+];
