749f912ca8f9c931e405ebdf15752eca71bde29e
frog <dickycahyakurniawan98@gmail.com>
2025-09-13 20:50:27 +0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -2,19 +2,19 @@ const movies = [
   {
     title: "Legend of Hero 1",
     genre: "action",
-    poster: "posters/movie1.jpg",
+    poster: "assets/poster/hero-returns.svg",
     url: "https://youtu.be/dQw4w9WgXcQ"
   },
   {
     title: "Romance in Paris",
     genre: "romance",
-    poster: "posters/movie2.jpg",
+    poster: "assets/poster/love-in-sky.svg",
     url: "https://youtu.be/dQw4w9WgXcQ"
   },
   {
     title: "Dragon Fantasy",
     genre: "fantasy",
-    poster: "assets/posters/renegade-immortal.svg",
+    poster: "assets/poster/renegade-immortal.svg",
     url: "https://youtu.be/dQw4w9WgXcQ"
   }
   // tambahin seterusnya...
